b17e9ca8bf724e6dbee5ac839241e5dd00990f79
Thiago Rodrigues <Thiago Rodrigues@192.168.1.227>
2025-03-30 17:16:48 -0300

    Primeiro commit

---
+++ santandern-dev-week/src/test/java/projeto/dio/santandern/dev/week/SantandernDevWeekApplicationTests.java
@@ -0,0 +1,13 @@
+package projeto.dio.santandern.dev.week;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantandernDevWeekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
